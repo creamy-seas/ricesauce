94a9f6c45a1c4d566aefb2b211294e76b29eae9c
creamy-seas <ilya.antonov.2013@live.rhul.ac.uk>
2019-03-29 18:10:13 +0000

    fixed boomer youtube save

--- scripts/boomer_usa.sh
+++ scripts/boomer_usa.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 #Connect to lukes channel and steam the shitpost
-cd /home/antonov/creamy_seas/video_tutorials/linux;
+cd /home/antonov/creamy_seas/boomer/linux;
 
 
-youtube-dl -f 135+140 -i --playlist-end 10  https://www.youtube.com/channel/UC2eYFnH61tmytImy1mTYvhA;
+youtube-dl -f 135+140 -i --playlist-end 10 -o \"%(title)s.%(ext)s\"  https://www.youtube.com/channel/UC2eYFnH61tmytImy1mTYvhA;
 
 echo "====> videos synced";
 yandex-disk sync
