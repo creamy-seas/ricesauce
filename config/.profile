ce08b2180934e4340abe3f2066bb2878dea095dc
goodgoy <ilya.antonov.2013@live.rhul.ac.uk>
2019-04-01 11:42:42 +0100

    Sorted out services and timers - need full path

--- config/.profile
+++ config/.profile
@@ -1,1 +1,2 @@
 alias yt="youtube-dl -f 135+140 -o \"%(title)s.%(ext)s\""
+alias srcp="source ~/creamy_seas/syncFiles/python_vi/arch_vi/bin/activate"
--- scripts/boomer_usa.sh
+++ scripts/boomer_usa.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-
+echo "==> Connect to Lukes channel and stream the shitpost"
 #Connect to lukes channel and steam the shitpost
 cd /home/antonov/creamy_seas/boomer/linux;
 
 
-youtube-dl -f 135+140 -i --playlist-end 10 -o \"%(title)s.%(ext)s\"  https://www.youtube.com/channel/UC2eYFnH61tmytImy1mTYvhA;
+youtube-dl -f 135+140 -i --playlist-end 10 -o "%(title)s.%(ext)s"  https://www.youtube.com/channel/UC2eYFnH61tmytImy1mTYvhA;
 
 echo "====> videos synced";
 yandex-disk sync
+yandex-disk start
 echo "====> yandex synced"
