42f0084657f4f735f3a0a51cd7fd8f4331d73a9e
goodgoy <ilya.antonov.2013@live.rhul.ac.uk>
2019-04-06 08:07:18 +0100

    matplotlibfile hardlinked

--- scripts/boomer_usa.sh
+++ scripts/boomer_usa.sh
@@ -4,7 +4,11 @@ echo "==> Connect to Lukes channel and stream the shitpost"
 cd /home/antonov/creamy_seas/boomer/linux;
 youtube-dl -f 135+140 -i --playlist-end 10 -o "%(title)s.%(ext)s"  https://www.youtube.com/channel/UC2eYFnH61tmytImy1mTYvhA;
 
-# download from my file
+echo "==> Connect to Varg and white children"
+cd /home/antonov/creamy_seas/boomer/varg;
+youtube-dl -f 135+140 -i --playlist-end 10 -o "%(title)s.%(ext)s" https://www.youtube.com/user/ThuleanPerspective
+
+#download from my file
 videoFileLocation="/home/antonov/creamy_seas/syncFiles/downloader.txt"
 cd /home/antonov/creamy_seas/boomer
 while read line; do
