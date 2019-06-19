7937a6cd9c37c6043c72f839acfa5ff67fb5df40
thinkpad chad <ilya.antonov.2013@live.rhul.ac.uk>
2019-06-19 10:05:23 +0100

    thinkpad iteration_1

--- ilya_install.sh
+++ ilya_install.sh
@@ -1,4 +1,5 @@
 echo "==> Syncing files from ricing folder"jjjkk
+
 # 1 - copy configuration files, some as hard links
 echo "  > COPYING configuration files"
 cd config
@@ -7,28 +8,29 @@ cp -fv remaps ~/.scripts/tools/
 # 1b - copy bash profile
 cat .profile >> ~/.profile
 source ~/.profile
+
 # 2 - create hard links to scripts (so that if they change, they also update in this folder)
-echo "  > LINKING scripts to .ilya_scripts directory"
-cd ../scripts
-mkdir -p  ~/.ilya_scripts
-ln -fv * ~/.ilya_scripts/
-
-# 3 - copy over netstat
-echo "  > COPYING netstat settings to /etc/netctl"
-cd ../netstat
-sudo cp -fv * /etc/netctl
-
-# 4 - copy systemctl scripts
-echo "  > COPYING systemctl scripts to launch .ilya_scripts"
-cd ../systemctl
-sudo cp -fv * /etc/systemd/system/
-
-# 5 - yandex disk
-echo "  > COPYING yandex disk settings"
-cd ../yandex
-cp -fv * ~/.config/yandex-disk/
-
-# 6 - matplotlib
-echo "  > COPYING matplotlib settings"
-cd ..
-cp -r matplotlib ~/.config
+#echo "  > LINKING scripts to .ilya_scripts directory"
+#cd ../scripts
+#mkdir -p  ~/.ilya_scripts
+#ln -fv * ~/.ilya_scripts/
+#
+## 3 - copy over netstat
+#echo "  > COPYING netstat settings to /etc/netctl"
+#cd ../netstat
+#sudo cp -fv * /etc/netctl
+#
+## 4 - copy systemctl scripts
+#echo "  > COPYING systemctl scripts to launch .ilya_scripts"
+#cd ../systemctl
+#sudo cp -fv * /etc/systemd/system/
+#
+## 5 - yandex disk
+#echo "  > COPYING yandex disk settings"
+#cd ../yandex
+#cp -fv * ~/.config/yandex-disk/
+#
+## 6 - matplotlib
+#echo "  > COPYING matplotlib settings"
+#cd ..
+#cp -r matplotlib ~/.config
