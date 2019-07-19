ebb3eb32fe8d65a85acee58313d3ab18b0305bcc
thinkpad chad <ilya.antonov.2013@live.rhul.ac.uk>
2019-07-19 16:53:38 +0000

    systemctl for ssh

--- .local/bin/mypy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-source /home/antonov/creamy_seas/syncFiles/python_vi/arch_vi/bin/activate
-cd /home/antonov/creamy_seas/monday_starts_on_saturday/dipole/python/spectrum
-python twinAnneal.py
